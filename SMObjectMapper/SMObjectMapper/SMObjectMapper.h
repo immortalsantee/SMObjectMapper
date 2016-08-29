471ef0f5783de0568e421d098bbf2c9b0038190a
Santosh Maharjan <immortalsantee@gmail.com>
2016-08-29 12:19:03 +0545

    initial commit

---
+++ SMObjectMapper/SMObjectMapper/SMObjectMapper.h
@@ -0,0 +1,19 @@
+//
+//  SMObjectMapper.h
+//  SMObjectMapper
+//
+//  Created by Santosh Maharjan on 8/29/16.
+//  Copyright © 2016 Cyclone Nepal Info Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMObjectMapper.
+FOUNDATION_EXPORT double SMObjectMapperVersionNumber;
+
+//! Project version string for SMObjectMapper.
+FOUNDATION_EXPORT const unsigned char SMObjectMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMObjectMapper/PublicHeader.h>
+
+
